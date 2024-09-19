2941069c6ac2ca2a0a9006d6daab98815a520024
Akira-Yip <tcye@ucdavis.edu>
2024-09-18 23:28:01 -0700

    add links to landing page

--- src/Kanbas/index.tsx
+++ src/Kanbas/index.tsx
@@ -6,6 +6,14 @@ import Courses from "./Courses";
 export default function Kanbas() {
     return (
         <div id="wd-kanbas">
+            <h6>Kira Ye SEC01</h6>
+            <a href="https://github.com/Akira-Yip/kanbas-react-web-app/tree/a1">Visit the a1 branch of Kanbas React Web App on GitHub</a>
+            <p>
+            </p>
+            <a href="https://a1--playful-cascaron-dcee19.netlify.app/#/Labs/Lab1">Visit the Labs here</a>
+            <p>
+            </p>
+
             <table>
                 <tr>
                     <td valign="top">
