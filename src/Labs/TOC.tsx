6a74b82563c189f04240e0af969ff0eb7e417ed0
Akira-Yip <tcye@ucdavis.edu>
2024-09-18 23:10:08 -0700

    added name

--- src/Labs/TOC.tsx
+++ src/Labs/TOC.tsx
@@ -2,12 +2,17 @@ import { Link } from "react-router-dom";
 
 export default function TOC() {
   return (
-    <ul>
-      <li><Link to="/Labs">Labs</Link></li>
-      <li><Link to="/Labs/Lab1">Lab 1</Link></li>
-      <li><Link to="/Labs/Lab2">Lab 2</Link></li>
-      <li><Link to="/Labs/Lab3">Lab 3</Link></li>
-      <li><Link to="/Kanbas">Kanbas</Link></li>
-    </ul>
+    <div>
+      <h3>Kira Ye SEC 01</h3>
+      <a href="https://github.com/Akira-Yip/kanbas-react-web-app/tree/a1">Visit the a1 branch of Kanbas React Web App on GitHub</a>
+
+      <ul>
+        <li><Link to="/Labs">Labs</Link></li>
+        <li><Link to="/Labs/Lab1">Lab 1</Link></li>
+        <li><Link to="/Labs/Lab2">Lab 2</Link></li>
+        <li><Link to="/Labs/Lab3">Lab 3</Link></li>
+        <li><Link to="/Kanbas">Kanbas</Link></li>
+      </ul>
+    </div>
   );
 }
